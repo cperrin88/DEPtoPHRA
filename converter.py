ae8d6f86bc94e8e797d39c1c88ef2a9bda46c55a
Christopher Perrin <cperrin@screebo.net>
2014-11-15 14:23:34 +0100

    Working projections

--- converter.py
+++ converter.py
@@ -10,48 +10,62 @@ class Converter(object):
         self.arg_table = arg_table
         self.mod_table = mod_table
 
-    def convert(self):
         self.head_tree = self._make_projection(self.dep_graph.root)
+
+    def convert(self):
         self._convert_subtree(self.dep_graph.root['deps'])
 
-    def _convert_subtree(self, nodelist, suptree=None):
+    def _convert_subtree(self, nodelist, suptree=None, supnode=None):
         if len(nodelist) == 0:
             return
 
         for node in nodelist:
             n = self.dep_graph.get_by_address(node)
-            proj = self._make_projection(n, suptree)
-            self._connect_to_head(proj)
-            self._convert_subtree(n['deps'], proj)
+            proj = self._make_projection(n, suptree, supnode)
+            self._convert_subtree(n['deps'], proj, n)
+            if suptree is None:
+                self._connect_to_head(proj)
 
-    def _make_projection(self, node, suptree=None):
+    def _make_projection(self, node, suptree=None, supnode=None):
         out = SearchableTree(node['word'], list())
         elem = node['tag']
 
-        while elem in self.proj_table.keys():
+        while True:
             out = SearchableTree(elem, [out])
+            if not suptree is None:
+                f = suptree.find_lowest(elem)
+                if f != "":
+                    if supnode['address'] < node['address']:
+                        f.append(out)
+                    else:
+                        f.insert(-1, out)
+                    out = f
+                    break
+
+            if not elem in self.proj_table.keys():
+                break
+
             elem = self.proj_table[elem]
 
-        if not suptree is None:
-            pass
-        return SearchableTree(elem, [out])
+        return out
 
-    def _connect_to_head(self, subtree):
-        pass
+    def _connect_to_head(self, subtree, table):
+        print(subtree.pprint())
 
 
 class SearchableTree(tree.Tree):
-
     def find(self, string):
         if self.label() == string:
             return self
-        for leave in self.leaves():
+        for leave in self:
             return leave.find(string)
         return ""
 
     def find_lowest(self, string):
-        for leave in self.leaves():
-            return leave.find(string)
+        for leave in self:
+            f = leave.find(string)
+            if f != "":
+                return f
         if self.label() == string:
             return self
         return ""
