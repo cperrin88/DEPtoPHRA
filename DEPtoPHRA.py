bbb8bc478aa2a3038294a997283a84ba4b404cbb
Christopher Perrin <cperrin@screebo.net>
2014-11-16 02:30:34 +0100

    Added some new arguments for output

--- DEPtoPHRA.py
+++ DEPtoPHRA.py
@@ -8,6 +8,7 @@ from nltk import parse
 import argparse
 import json
 import converter
+import os
 
 
 def main():
@@ -16,16 +17,29 @@ def main():
     parser.add_argument("projections", type=argparse.FileType(mode="r"), help="Projections in JSON format")
     parser.add_argument("arguments", type=argparse.FileType(mode="r"), help="Argument table in JSON format")
     parser.add_argument("modifiers", type=argparse.FileType(mode="r"), help="Modification table in JSON format")
+    parser.add_argument("-d", "--draw", action='store_true', help="Draw the graph (windowmanager needed)")
+    parser.add_argument("-p", "--print", action='store_true', help="Print to the console")
+    parser.add_argument("-q", "--qtree", action='store_true', help="Print latex qTree to the console")
+    parser.add_argument("-o", "--outfile", dest="outfile", type=argparse.FileType(mode="w"), help="Write tree to file")
     args = parser.parse_args()
 
     deps = parse.DependencyGraph.load(args.data)
     proj = json.load(args.projections)
     arg = json.load(args.arguments)
     mod = json.load(args.modifiers)
+
     for dep in deps:
         c = converter.Converter(dep, proj, arg, mod)
         ret = c.convert()
-        ret.draw()
-
+        if args.print:
+            print(ret.pprint())
+        if args.qtree:
+            print(ret.pprint_latex_qtree())
+        if args.outfile:
+            args.outfile.write(ret.pprint())
+            args.outfile.write(os.linesep)
+            args.outfile.flush()
+        if args.draw:
+            ret.draw()
 if __name__ == '__main__':
     main()
